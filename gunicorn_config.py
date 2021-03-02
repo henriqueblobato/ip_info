5ce5fb5a26bbb81e35025fd7df6a2cc04219fa7b
ik lobato <hlobato@blacklinesafety.com>
2021-03-01 23:03:05 -0300

    remove heroku unused

--- gunicorn_config.py
+++
@@ -1,8 +0,0 @@
-from settings import *
-bind = f"0.0.0.0:{APP_PORT}"
-workers = GUNICORN_WORKERS
-threads = GUNICORN_THREADS
-timeout = GUNICORN_TIMEOUT
-limit_request_line = 4094
-limit_request_fields = 16
-limit_request_field_size = 8190
